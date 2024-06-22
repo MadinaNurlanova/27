002a790b5f623d1e115c87e61d950d2c11afbdbe
Madina <you@example.com>
2024-06-22 17:18:21 +0600

    Навигация

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { First, Second, Third, Fourth } from './pages'
 import { Link } from 'react-router-dom';
 
+
 function App() {
 
   return (
